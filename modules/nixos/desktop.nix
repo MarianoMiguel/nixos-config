9fc59d4c9bf6e47b76b13bd77da26212c55b6163
Mariano Miguel <mariano@mariano.io>
2026-05-22 01:35:52 -0300

    Add KDE shortcuts and disable animations

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,3 +1,8 @@
+{ pkgs, ... }:
+
+let
+  kwriteconfig = "${pkgs.kdePackages.kconfig}/bin/kwriteconfig6";
+in
 {
   services.xserver.enable = true;
 
@@ -19,4 +24,19 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  system.activationScripts.kdeShortcuts.text = ''
+    install -d -m 0755 -o mariano -g users /home/mariano/.config
+
+    ${kwriteconfig} --file /home/mariano/.config/kglobalshortcutsrc --group kwin --key "Window Close" "Alt+Q\tAlt+W\tAlt+F4,Alt+Q\tAlt+W\tAlt+F4,Close Window"
+    ${kwriteconfig} --file /home/mariano/.config/kglobalshortcutsrc --group kwin --key "Window Fullscreen" "Alt+Enter,Alt+Enter,Make Window Fullscreen"
+    ${kwriteconfig} --file /home/mariano/.config/kdeglobals --group KDE --key AnimationDurationFactor 0
+
+    chown mariano:users /home/mariano/.config/kglobalshortcutsrc
+    chown mariano:users /home/mariano/.config/kdeglobals
+  '';
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.kconfig
+  ];
 }

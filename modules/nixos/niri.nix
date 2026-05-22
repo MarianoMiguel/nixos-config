d2442b8f28c24497fcc46a47c44071b0f77f4feb
Mariano Miguel <mariano@mariano.io>
2026-05-22 05:04:41 -0300

    Sync DMS theme with session color scheme

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -2,6 +2,21 @@
 
 let
   system = pkgs.stdenv.hostPlatform.system;
+  dmsShell = dms.packages.${system}.dms-shell.overrideAttrs (old: {
+    postInstall = old.postInstall + ''
+      substituteInPlace $out/share/quickshell/dms/Common/Theme.qml \
+        --replace-fail '        Quickshell.execDetached(["mkdir", "-p", stateDir]);' '        Quickshell.execDetached(["mkdir", "-p", stateDir]);
+        syncSystemColorScheme(isLightMode);' \
+        --replace-fail '    function setLightMode(light, savePrefs = true, enableTransition = false) {' '    function syncSystemColorScheme(light) {
+        const scheme = light ? "prefer-light" : "prefer-dark";
+        Proc.runCommand("setSystemColorScheme", ["${pkgs.glib}/bin/gsettings", "set", "org.gnome.desktop.interface", "color-scheme", scheme], () => {});
+    }
+
+    function setLightMode(light, savePrefs = true, enableTransition = false) {
+        syncSystemColorScheme(light);
+'
+    '';
+  });
 in
 
 {
@@ -9,7 +24,7 @@ in
 
   programs.dank-material-shell = {
     enable = true;
-    package = dms.packages.${system}.dms-shell;
+    package = dmsShell;
     quickshell.package = pkgsUnstable.quickshell;
 
     # dgop is not available in the current nixpkgs pin.
